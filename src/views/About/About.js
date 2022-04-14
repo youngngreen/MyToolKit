2d9872ccc9255327a24fbcda21ad4284c42cab85
youngngreen <lenamtienthanh@gmail.com>
2022-04-15 00:52:23 +0700

    update about

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -4,7 +4,7 @@ class About extends React.Component {
     render () {
         return (
             <div>
-                about
+                Hi my name is Thanh Le
             </div>
         )
     }
